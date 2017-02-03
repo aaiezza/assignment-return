cf035d80bfea124f50c694941a24e6192c64f707
Alex Aiezza <Alex Aiezza@BIO-W7-3FGZPD2.UR.Rochester.edu>
2017-02-03 18:55:58 -0500

    Initialize Eclipse Project

---
+++ src/edu/rochester/bio/ar/AssignmentReturn.java
@@ -0,0 +1,20 @@
+package edu.rochester.bio.ar;
+
+/**
+ * @author Alex Aiezza
+ *
+ */
+public class AssignmentReturn
+{
+    
+    
+
+    /**
+     * @param args
+     */
+    public static void main( String [] args )
+    {
+
+    }
+
+}
