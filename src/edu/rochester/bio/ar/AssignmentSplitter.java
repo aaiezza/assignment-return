43e0abe433328798c1da030eeb7f3c0070c71862
Alex Aiezza <Alex Aiezza@flik-pc>
2017-02-05 04:24:46 -0500

    Progress on AssignmentSplitter

--- src/edu/rochester/bio/ar/AssignmentSplitter.java
+++ src/edu/rochester/bio/ar/AssignmentSplitter.java
@@ -3,6 +3,10 @@
  */
 package edu.rochester.bio.ar;
 
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
 /**
  * TODO Use the Apace PDFBox API for manipulating the combined assignment PDF
  * 
@@ -11,5 +15,45 @@ package edu.rochester.bio.ar;
  */
 public class AssignmentSplitter
 {
+    private final PDDocument                     combinedAssignment;
+
+    private final File                           outputDirectory;
+
+    private final AssignmentReturnerInterpolator ari;
+
+    /**
+     * @param combinedAssignment
+     *            The entire PDF file for all students for this single
+     *            assignment
+     * @param outputDirectory
+     *            The directory to output all of the individual PDF files
+     * @param ari
+     *            The PDF name interpolator
+     */
+    public AssignmentSplitter(
+        PDDocument combinedAssignment,
+        File outputDirectory,
+        AssignmentReturnerInterpolator ari )
+    {
+        super();
+        this.combinedAssignment = combinedAssignment;
+        this.outputDirectory = outputDirectory;
+        this.ari = ari;
+    }
+
+    public void split()
+    {
+        // Ensure the output directory exists
+        if ( !outputDirectory.exists() )
+            outputDirectory.mkdirs();
+
+        final int assignmentLength = combinedAssignment.getNumberOfPages() /
+                ari.getRoster().rowMap().size();
+
+        ari.convert().forEach( fileName -> {
+            // Splitter
+        } );
+
+    }
 
 }
