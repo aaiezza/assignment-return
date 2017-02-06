e946bdc0d5c0f1bfb97893d195c04c1c52efb951
Alex Aiezza <Alex Aiezza@BIO-W7-3FGZPD2.UR.Rochester.edu>
2017-02-06 11:41:38 -0500

    Update AssignmentPreviewer
    Moved Previewer into its own package

---
+++ src/edu/rochester/bio/ar/preview/AssignmentPreviewer.java
@@ -0,0 +1,65 @@
+/**
+ *  COPYRIGHT (C) 2017 Alex Aiezza. All Rights Reserved.
+ */
+package edu.rochester.bio.ar.preview;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.io.File;
+
+import javax.swing.JFrame;
+
+import com.google.common.collect.Table;
+
+/**
+ * TODO Will have some form of JFrame to show assignments.
+ * 
+ * @author Alex Aiezza
+ *
+ */
+public class AssignmentPreviewer extends JFrame
+{
+    private static final long                    serialVersionUID = 1L;
+
+    private final Table<Integer, String, String> roster;
+    private final File                           pdfDirectory;
+    private final String                         assignment;
+
+    /**
+     * @param roster
+     * @param pdfDirectory
+     * @throws HeadlessException
+     */
+    public AssignmentPreviewer(
+        Table<Integer, String, String> roster,
+        File pdfDirectory,
+        String assignment ) throws HeadlessException
+    {
+        super();
+        this.roster = roster;
+        this.pdfDirectory = pdfDirectory;
+        this.assignment = assignment;
+
+        initJFrame();
+    }
+
+    private void initJFrame()
+    {
+        setTitle( String.format( "AssignmentReturner Preview: %s", assignment ) );
+        final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize( dim.width * 3 / 5, dim.height * 4 / 5 );
+        setLocation( dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
+        setVisible( true );
+        setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+
+        getContentPane().setBackground( new Color( 20, 20, 70 ) );
+
+        // TODO Set the border layout
+
+        // TODO Create and add the StudentAssignmentConfirmer JPanel to left
+
+        // TODO Create and add AssignmentPDFViewer JPanel to the right
+    }
+}
---
+++ src/edu/rochester/bio/ar/preview/StudentAssignmentConfirmer.java
@@ -0,0 +1,40 @@
+/**
+ *  COPYRIGHT (C) 2017 Alex Aiezza. All Rights Reserved.
+ */
+package edu.rochester.bio.ar.preview;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.swing.JPanel;
+
+import com.google.common.collect.Table;
+
+/**
+ * @author Alex Aiezza
+ *
+ */
+/**
+ * @author Alex Aiezza
+ *
+ */
+public class StudentAssignmentConfirmer
+{
+    private static final long                    serialVersionUID = 1L;
+    private final Table<Integer, String, String> roster;
+
+
+    /**
+     * This integer is the current index of the roster for the student that is
+     * being confirmed to he linked to the currently displayed PDF
+     */
+    private final AtomicInteger                  confirmingStudent;
+
+    /**
+     * 
+     */
+    public StudentAssignmentConfirmer( final Table<Integer, String, String> roster )
+    {
+        this.roster = roster;
+        confirmingStudent = new AtomicInteger();
+    }
+}
